d512ae94087ca07030200a13a9c2c621a78a6511
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-16 11:22:36 +0500

    s

--- OddNumbersRange.py
+++ OddNumbersRange.py
@@ -1,4 +1,7 @@
+# Taking input from the user for lower limit of the range.
 lower=int(input("Enter the lower limit for the range: "))
+
+
 upper=int(input("Enter the upper limit for the range: "))
 for i in range(lower,upper+1):
     if(i%2!=0):
